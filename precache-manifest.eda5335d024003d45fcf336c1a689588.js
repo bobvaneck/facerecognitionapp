86d725119c8f1e01bc02a3bdea5e81a06f85ccea
bobvaneck <bobvaneck@gmail.com>
2020-07-14 11:15:21 +1000

    Updates

---
+++ precache-manifest.eda5335d024003d45fcf336c1a689588.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "483bf1052eacfc9c1d82f166def33e80",
+    "url": "/bobvaneck/facerecognitionapp/index.html"
+  },
+  {
+    "revision": "a3e309f040829b7a88fa",
+    "url": "/bobvaneck/facerecognitionapp/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "639f332a9c50e2f1a334",
+    "url": "/bobvaneck/facerecognitionapp/static/css/main.0baa4fe8.chunk.css"
+  },
+  {
+    "revision": "a3e309f040829b7a88fa",
+    "url": "/bobvaneck/facerecognitionapp/static/js/2.9c6b30dd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bobvaneck/facerecognitionapp/static/js/2.9c6b30dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "639f332a9c50e2f1a334",
+    "url": "/bobvaneck/facerecognitionapp/static/js/main.65eca85f.chunk.js"
+  },
+  {
+    "revision": "745b7b6787a2b22aa36c",
+    "url": "/bobvaneck/facerecognitionapp/static/js/runtime-main.65f32503.js"
+  },
+  {
+    "revision": "f9fa16e6e77da194e67337a3a438fa61",
+    "url": "/bobvaneck/facerecognitionapp/static/media/Logo.f9fa16e6.png"
+  }
+]);
